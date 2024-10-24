cd0f2e21e2c4fb2771d584d0d74e2af6ada4e4b0
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-10-24 12:19:24 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
+
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  const [posts, setPosts] = useState([]);
+  // Fetch data from the API
+  useEffect(() => {
+    async function fetchPosts() {
+      const url =
+        "https://react-user-crud-app-default-rtdb.firebaseio.com/posts.json";
+      const response = await fetch(url);
+      const data = await response.json(); // JSON.parse(response)
+      console.log(data);
+      // from object to array
+      const postsArray = Object.keys(data).map(postId => ({
+        id: postId,
+        ...data[postId]
+      }));
+      console.log(postsArray);
+      setPosts(postsArray);
+    }
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="page">
+      <div className="grid">
+        {posts.map(post => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 }
