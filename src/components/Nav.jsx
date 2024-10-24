e43b3aa222e71489e007719e7450ba4ccb24ed9f
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-10-24 12:19:46 +0200

    Refactor routing and navigation components
    - Update App.jsx to include a new route for creating posts
    - Update Nav.jsx to add a link for creating posts
    - Remove AboutPage.jsx and ContactPage.jsx as they are no longer needed
    - Add PostCard.jsx and UserAvatar.jsx components for displaying post information

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
-    return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-        </nav>
-    );
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/create">Create Post</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </nav>
+  );
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,13 @@
+import UserAvatar from "./UserAvatar";
+
+export default function PostCard({ post }) {
+  console.log(post);
+
+  return (
+    <article key={post.id} className="post-card">
+      <UserAvatar uid={post.uid} />
+      <img src={post.image} alt={post.caption} />
+      <h3>{post.caption}</h3>
+    </article>
+  );
+}
